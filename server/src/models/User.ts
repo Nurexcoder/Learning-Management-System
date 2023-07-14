163ad671c51ec0f3ce0a39832f4353518156b0f7
Hibjul Ahmed <hibjulahmed@gmail.com>
2023-07-14 10:18:42 +0530

    Sign up route

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -25,6 +25,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    name: {
+      type: String,
+      required: true,
+    },
   },
   {
     toJSON: {
